bd86a5366148756fde528029116e05c05aab8110
Playrom <Playrom@users.noreply.github.com>
2019-02-03 00:34:28 +0100

    First Build

---
+++ build/precache-manifest.edaf3c0a7a2a85914e36bf8fcd8c5d93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "74664939657ddac395d0648c7e7e20c5",
+    "url": "/static/media/fi.74664939.jpg"
+  },
+  {
+    "revision": "f913be0aedc1d7e0f0d6",
+    "url": "/static/css/main.20e41477.chunk.css"
+  },
+  {
+    "revision": "218374c16857079bab2c",
+    "url": "/static/js/1.218374c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3dc4e6ef00c2cd8c2c99f907bee208ce",
+    "url": "/static/media/favicon.3dc4e6ef.ico"
+  },
+  {
+    "revision": "f913be0aedc1d7e0f0d6",
+    "url": "/static/js/main.f913be0a.chunk.js"
+  },
+  {
+    "revision": "89ada645e23985e69a67588d8afbbc57",
+    "url": "/static/media/fi.89ada645.png"
+  },
+  {
+    "revision": "c621734de5a2d4a1f8bccdb8f41c669f",
+    "url": "/static/media/lega.c621734d.png"
+  },
+  {
+    "revision": "537d431bba829eb8f8981344bf70aad6",
+    "url": "/static/media/leu.537d431b.png"
+  },
+  {
+    "revision": "236f96c4da2972ff49923ae7ab1dcb32",
+    "url": "/static/media/m5s.236f96c4.png"
+  },
+  {
+    "revision": "6b84bf12a6068fb2c769ede7d9ab415b",
+    "url": "/index.html"
+  }
+];
